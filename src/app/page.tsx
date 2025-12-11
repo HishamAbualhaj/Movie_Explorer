bab99b9534f04aeadc65ca2b3e499c383458a85c
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-11 23:37:01 +0200

    Combine landing page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import Categories from "@/components/layouts/Landing/Categories/Categories";
+import Devices from "@/components/layouts/Landing/Devices/Devices";
+import FAQ from "@/components/layouts/Landing/FAQ/FAQ";
 import Hero from "@/components/layouts/Landing/Hero";
+import Plans from "@/components/layouts/Landing/Plans/Plans";
+import Trial from "@/components/ui/Trial";
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen">
-        <Hero />
-      </div>
+      <Hero />
+      <Categories />
+      <Devices />
+      <FAQ />
+      <Plans />
+      <Trial />
     </>
   );
 }
