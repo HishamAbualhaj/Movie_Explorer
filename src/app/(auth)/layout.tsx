a712585303a863165de02d61d9969b170a5ade3e
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-13 20:46:35 +0200

    Adjust Ui design for login and auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-screen">{children}</div>;
+  return <div className="">{children}</div>;
 };
 
 export default AuthLayout;
