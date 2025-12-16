03de176823d3a6f55cc773f6c3d9ce76750f8bf7
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-16 08:05:30 +0200

    Add placeholder data for movies

---
+++ src/data/Movies.ts
@@ -0,0 +1,114 @@
+import { Movie } from "@/types/movie";
+
+const movies: Movie[] = [
+  {
+    id: "tt1375666",
+    duration: 148,
+    title: "Inception",
+    overview:
+      "A skilled thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+    poster: "/movie_image.png",
+    year: 2010,
+    rating: 8.8,
+    genre: ["Action", "Sci-Fi", "Thriller"],
+  },
+  {
+    id: "tt0816692",
+    duration: 169,
+    title: "Interstellar",
+    overview:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity’s survival.",
+    poster: "/interstellar.jpeg",
+    year: 2014,
+    rating: 7,
+    genre: ["Adventure", "Drama", "Sci-Fi"],
+  },
+  {
+    id: "tt0468569",
+    duration: 152,
+    title: "The Dark Knight",
+    overview:
+      "Batman faces the Joker, a criminal mastermind who plunges Gotham City into chaos.",
+    poster: "/movie_image.png",
+    year: 2008,
+    rating: 8,
+    genre: ["Action", "Crime", "Drama"],
+  },
+  {
+    id: "tt0133093",
+    duration: 136,
+    title: "The Matrix",
+    overview:
+      "A computer hacker learns about the true nature of his reality and his role in the war against its controllers.",
+    poster: "/movie_image.png",
+    year: 1999,
+    rating: 7,
+    genre: ["Action", "Sci-Fi"],
+  },
+  {
+    id: "tt0114369",
+    duration: 127,
+    title: "Se7en",
+    overview:
+      "Two detectives hunt a serial killer who uses the seven deadly sins as his motives.",
+    poster: "/movie_image.png",
+    year: 1995,
+    rating: 10,
+    genre: ["Crime", "Drama", "Thriller"],
+  },
+  {
+    id: "tt0109830",
+    duration: 142,
+    title: "Forrest Gump",
+    overview:
+      "The story of a man with a low IQ who witnesses and influences several historical events.",
+    poster: "/movie_image.png",
+    year: 1994,
+    rating: 8.8,
+    genre: ["Drama", "Romance"],
+  },
+  {
+    id: "tt0120737",
+    duration: 178,
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    overview:
+      "A young hobbit begins a journey to destroy a powerful ring and save Middle-earth.",
+    poster: "/movie_image.png",
+    year: 2001,
+    rating: 8.8,
+    genre: ["Adventure", "Fantasy"],
+  },
+  {
+    id: "tt6751668",
+    duration: 132,
+    title: "Parasite",
+    overview:
+      "Greed and class discrimination threaten the newly formed symbiotic relationship between two families.",
+    poster: "/movie_image.png",
+    year: 2019,
+    rating: 8.5,
+    genre: ["Drama", "Thriller"],
+  },
+  {
+    id: "tt7286456",
+    duration: 122,
+    title: "Joker",
+    overview:
+      "A mentally troubled comedian embarks on a downward spiral that leads to chaos in Gotham City.",
+    poster: "/movie_image.png",
+    year: 2019,
+    rating: 8.4,
+    genre: ["Crime", "Drama", "Thriller"],
+  },
+  {
+    id: "tt4154796",
+    duration: 181,
+    title: "Avengers: Endgame",
+    overview:
+      "The Avengers assemble once more to reverse Thanos’ actions and restore balance to the universe.",
+    poster: "/movie_image.png",
+    year: 2019,
+    rating: 8.4,
+    genre: ["Action", "Adventure", "Sci-Fi"],
+  },
+];
