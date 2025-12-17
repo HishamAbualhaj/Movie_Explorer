cc4b129941af2c15321960122551f774191f4ec7
Omar Zuhri <omar.z.zuhri@gmail.com>
2025-12-17 19:40:42 +0200

    Cleaning the code by removing non-used lines.

--- src/app/(root)/novabot/page.tsx
+++ src/app/(root)/novabot/page.tsx
@@ -1,6 +1,4 @@
 import ChatLayout from "@/components/layouts/NovaBot/ChatLayout";
-import ChatSidebar from "@/components/layouts/NovaBot/ChatSidebar";
-import ChatWindow from "@/components/layouts/NovaBot/ChatWindow";
 
 export default function NovabotPage() {
   return (
