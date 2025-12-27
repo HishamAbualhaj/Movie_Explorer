63ac225ee06181d99e2cd5cf04d001efccd38bad
lelyan-salah <salahlelyan@gmail.com>
2025-12-27 22:15:04 +0200

    Update movie components and add Details section

--- src/app/(root)/show/[id]/page.tsx
+++ src/app/(root)/show/[id]/page.tsx
@@ -1,13 +1,8 @@
 import ShowOpenPage from "@/components/layouts/MoviePage/ShowOpenPage";
 import { ShowPageProps } from "@/types/movie";
 
-export default async function ShowPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+export default async function ShowPage({ params }: { params: Promise<{ id: string }> }) {
   const showId = (await params).id;
-  const isSubscribed = true;
 
-  return <ShowOpenPage isSubscribed={isSubscribed} />;
+  return <ShowOpenPage />;
 }
