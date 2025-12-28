0e34fde813572dea0137401bc538bc4d686c9c4e
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-28 10:44:17 +0200

    Fetch user data for dashboard

--- src/components/layouts/dashboard/UsersPage.tsx
+++ src/components/layouts/dashboard/UsersPage.tsx
@@ -2,37 +2,27 @@
 import Button from "@/components/ui/Button";
 import DataTable from "@/components/ui/DataTable";
 import Input from "@/components/ui/Input";
+import { supabase } from "@/supabase/client";
 import { Column, User } from "@/types";
+import { useQuery } from "@tanstack/react-query";
 import { X } from "lucide-react";
 import { useMemo, useState } from "react";
 
 const UsersPage = () => {
-  const users: User[] = [
-    {
-      id: '1',
-      name: "John Carter",
-      email: "john@mail.com",
-      plan: "Premium",
-      phone: "+1 555 123",
-      moviesWatched: 42,
-    },
-    {
-      id: '2',
-      name: "Sarah Smith",
-      email: "sarah@mail.com",
-      plan: "Free",
-      phone: "+44 222 333",
-      moviesWatched: 8,
-    },
-    {
-      id: '3',
-      name: "Ahmed Ali",
-      email: "ahmed@mail.com",
-      plan: "Premium",
-      phone: "+970 599 111",
-      moviesWatched: 120,
-    },
-  ];
+  const fetchUsers = async () => {
+    let query = supabase.from("users").select("*");
+
+    const { data, error } = await query;
+
+    if (error) throw error;
+    return data;
+  };
+
+  const { data: users, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => fetchUsers(),
+    staleTime: 1000 * 60 * 5,
+  });
 
   const columns: Column<User>[] = [
     { key: "name", label: "Name" },
@@ -67,11 +57,16 @@ const UsersPage = () => {
     },
   ];
 
+  const banUser = (user: User | null) => {
+    if (!user) return;
+
+    // handle ban user 
+  };
   const [search, setSearch] = useState("");
   const [modal, setModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const filteredUsers = useMemo(() => {
-    return users.filter((u) =>
+    return users?.filter((u) =>
       `${u.name} ${u.email}`.toLowerCase().includes(search.toLowerCase())
     );
   }, [search]);
@@ -86,31 +81,45 @@ const UsersPage = () => {
         className="w-full max-w-sm rounded-lg border border-border bg-bg-dark px-4 py-2 text-text-main placeholder:text-text-muted outline-none focus:border-primary"
       />
       {modal && (
-        <div className="flex flex-col gap-5 bg-bg-dark p-5 w-fit rounded-md">
-          <div className="flex text-white justify-between items-center gap-10">
-            <div className="text-white">
-              Are you sure you want to ban <strong>{selectedUser?.name}</strong>
-              ?
+        <div
+          onClick={() => {
+            setModal(false);
+          }}
+          className="bg-bg-dark/10 w-full h-full top-0 grid place-items-center fixed left-1/2 -translate-x-1/2"
+        >
+          <div className="flex flex-col gap-5 bg-bg-dark p-5 w-fit rounded-md ">
+            <div className="flex text-white justify-between items-center gap-10">
+              <div className="text-white">
+                Are you sure you want to ban{" "}
+                <strong>{selectedUser?.name}</strong>?
+              </div>
+              <div
+                onClick={() => {
+                  setModal(false);
+                  setSelectedUser(null);
+                }}
+                className="bg-bg-light p-2 rounded-md"
+              >
+                <X size={18} />
+              </div>
             </div>
-            <div
-              onClick={() => {
-                setModal(false);
-                setSelectedUser(null);
-              }}
-              className="bg-bg-light p-2 rounded-md"
-            >
-              <X size={18} />
+            <div className="flex gap-3">
+              <Button
+                className="py-2!"
+                onClick={() => {
+                  banUser(selectedUser);
+                }}
+              >
+                Yes?
+              </Button>
+              <Button className="py-2!" variant="secondary">
+                No
+              </Button>
             </div>
-          </div>
-          <div className="flex gap-3">
-            <Button className="py-2!">Yes?</Button>
-            <Button className="py-2!" variant="secondary">
-              No
-            </Button>
           </div>
         </div>
       )}
-      <DataTable data={users} columns={columns} />
+      <DataTable data={filteredUsers ?? []} columns={columns} />
     </div>
   );
 };
