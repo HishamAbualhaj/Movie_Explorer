6b6f42f064b47b7a84e6d699e990b64e382b967a
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-28 07:41:17 +0200

    Responsive for sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import SideBar from "@/components/layouts/dashboard/SideBar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex gap-5">
+    <div className="flex max-lg:flex-col lg:gap-5 gap-0">
       <SideBar />
       <div className="flex-1"> {children}</div>
     </div>
--- src/components/layouts/dashboard/SideBar.tsx
+++ src/components/layouts/dashboard/SideBar.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Logo from "../../ui/Logo";
-import { ChartBarBig, MicrowaveIcon, User2 } from "lucide-react";
+import { BarChart2Icon, ChartBarBig, MicrowaveIcon, User2 } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const SideBar = () => {
   const sideItems = [
@@ -20,29 +22,51 @@ const SideBar = () => {
       icon: <MicrowaveIcon size={19} />,
     },
   ];
+  const [sideBar, setSideBar] = useState(false);
   return (
-    <div className="flex flex-col bg-bg-dark h-screen shadow-2xl">
-      <div className="flex-1">
-        <div className="py-4 px-8">
-          <Logo />
-        </div>
-        <div className="">
-          <div className="flex flex-col">
-            {sideItems.map((item) => (
-              <Link
-                href={item.link}
-                key={item.title}
-                className="flex items-center gap-2 text-text-secondary border-b py-5 border-border px-5 hover:bg-bg-light transition"
-              >
-                {item.icon}
-                {item.title}
-              </Link>
-            ))}
-          </div>
+    <>
+      <div className="px-5 lg:hidden">
+        <div
+          onClick={() => {
+            setSideBar(!sideBar);
+          }}
+          className=" text-white mt-5  bg-bg-light p-2 h-fit w-fit rounded-md hover:bg-bg-light/50 cursor-pointer"
+        >
+          <BarChart2Icon className="rotate-90" />
         </div>
       </div>
-      <Link href="/logout" className="text-white px-5 bg-primary/80 py-3 font-medium">Logout</Link>
-    </div>
+      <div
+        className={`max-lg:-translate-x-full transition max-lg:absolute max-lg:mt-18 max-lg:rounded-md z-10 flex flex-col bg-bg-dark lg:h-screen h-96 shadow-2xl ${
+          sideBar ? "translate-x-[calc(0%+20px)]!" : ""
+        } `}
+      >
+        <div className="flex-1">
+          <div className="py-4 px-8">
+            <Logo />
+          </div>
+          <div className="">
+            <div className="flex flex-col">
+              {sideItems.map((item) => (
+                <Link
+                  href={item.link}
+                  key={item.title}
+                  className="flex items-center gap-2 text-text-secondary border-b py-5 border-border px-5 hover:bg-bg-light transition"
+                >
+                  {item.icon}
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+        <Link
+          href="/logout"
+          className="text-white px-5 bg-primary/80 py-3 font-medium"
+        >
+          Logout
+        </Link>
+      </div>
+    </>
   );
 };
 
